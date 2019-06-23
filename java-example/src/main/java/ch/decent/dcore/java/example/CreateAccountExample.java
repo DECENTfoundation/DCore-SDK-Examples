ea4e96eb2b4ccd0744da6dc2239afc1a664c5b86
Mario Kapusta <mario.kapusta@ubisoft.com>
2019-06-23 16:13:25 +0100

    Integrated Kotlin Dcore sdk library and setuped compiiler. Implemented api init example, Implkemented login example and get balance.

---
+++ java-example/src/main/java/ch/decent/dcore/java/example/CreateAccountExample.java
@@ -0,0 +1,15 @@
+package ch.decent.dcore.java.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateAccountExample {
+
+    @Autowired
+    private GenerateKeyExample generateKeyExample;
+
+    public void createAccount() {
+
+    }
+}
